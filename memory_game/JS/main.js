dfd0b5e26f97946cdd4ac57cea35b9e29e075655
Garrett Moore <robertgarrettmoore@gmail.com>
2019-01-01 22:14:43 -0800

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,17 +1,19 @@
 let card = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = card[1];
-let cardTwo = card[3];
 
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
+let checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  alert("You found a match!");
+} else {
+	  alert("Sorry, try again.");
 	}
+};
+
+let flipCard = function (cardId) {
+	if (cardsInPlay.length === 2) {
+	checkForMatch ();
 }
-console.log("User flipped a " + cardTwo);
+	console.log("User flipped a " + card[cardId]);
+	cardsInPlay.push(card[cardId]);
+};
+
